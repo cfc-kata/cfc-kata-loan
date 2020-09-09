1d4b385c0f78246d9e805e8981f181092168ae12
Mei Xuesong <meixuesong@gmail.com>
2020-09-09 14:24:34 +0800

    init project

---
+++ src/test/java/com/cfckata/loan/LoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfckata.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
